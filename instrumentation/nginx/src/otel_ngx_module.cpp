9e86e7e101ddc040953533729db7f0649cda7763
wuchenghao <chenghao.wu@bangdao-tech.com>
2024-01-17 14:05:33 +0800

    feat: add service namespace

--- instrumentation/nginx/src/otel_ngx_module.cpp
+++ instrumentation/nginx/src/otel_ngx_module.cpp
@@ -1121,7 +1121,7 @@ static ngx_int_t OtelNgxStart(ngx_cycle_t* cycle) {
   auto provider =
     nostd::shared_ptr<opentelemetry::trace::TracerProvider>(new sdktrace::TracerProvider(
       std::move(processor),
-      opentelemetry::sdk::resource::Resource::Create({{"service.name", agentConf->service.name}}),
+      opentelemetry::sdk::resource::Resource::Create({{"service.namespace", "kube-system"}}),
       std::move(sampler)));
 
   opentelemetry::trace::Provider::SetTracerProvider(std::move(provider));
